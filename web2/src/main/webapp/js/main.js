8fa6276854fba709aaf46fcf0f2f181bece9139b
yaroslav.yermilov <yermilov17@gmail.com>
2014-01-12 22:53:37 +0200

    add diff operation to web2 project

--- web2/src/main/webapp/js/main.js
+++ web2/src/main/webapp/js/main.js
@@ -1,31 +1,53 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    loadTableList('#selectTable1', '#table1Data');
+    loadTableList('#selectTable2', '#table2Data');
+
+    prepareUniqueButton();
+    prepareDifferenceButton();
+});
+
+function loadTableList(selectOneSelector, tableSelector) {
     $.get("http://localhost:8080/kuzoff-ws/api/table", null,
-        function(responseData) {
-            $('#tablesTmpl').tmpl(responseData).appendTo('#selectTable');
-            loadDataForTable($('#selectTable').val());
+        function (responseData) {
+            $('#tablesTmpl').tmpl(responseData).appendTo(selectOneSelector);
+            loadDataForTable($(selectOneSelector).val(), tableSelector);
         }
     );
-    
-    $('#selectTable').on('change', function() {
-        loadDataForTable($(this).val());
+
+    $(selectOneSelector).on('change', function () {
+        loadDataForTable($(this).val(), tableSelector);
     });
-    
-    $("#buttonUnique").click(function(e){
-        $.post("http://localhost:8080/kuzoff-ws/api/table/" + $('#selectTable').val() + "/unique", null,
-            function(responseData) {
-                $('#tableData').empty();
-                $('#rowTmpl').tmpl(responseData).appendTo('#tableData');
+}
+
+function loadDataForTable(tableName, tableSelector) {
+    $.get("http://localhost:8080/kuzoff-ws/api/table/" + tableName + "/data", null,
+        function (responseData) {
+            $('#tableData').empty();
+            $('#rowTmpl').tmpl(responseData).appendTo(tableSelector);
+        }
+    );
+}
+
+function prepareUniqueButton() {
+    $("#buttonUnique").click(function (e) {
+        $.post("http://localhost:8080/kuzoff-ws/api/table/" + $('#selectTable1').val() + "/unique", null,
+            function (responseData) {
+                $('#table1Data').empty();
+                $('#rowTmpl').tmpl(responseData).appendTo('#table1Data');
             }
         );
         e.preventDefault();
-    }); 
-});
+    });
+}
 
-function loadDataForTable(tableName) {
-    $.get("http://localhost:8080/kuzoff-ws/api/table/" + tableName + "/data", null,
-        function(responseData) {
-            $('#tableData').empty();
-            $('#rowTmpl').tmpl(responseData).appendTo('#tableData');
-        }
-    );
+function prepareDifferenceButton() {
+    $("#buttonDifference").click(function (e) {
+        $.post("http://localhost:8080/kuzoff-ws/api/table/" + $('#selectTable1').val() + "/diff/" + $('#selectTable2').val(), null,
+            function (responseData) {
+                $('#table1Data').empty();
+                $('#rowTmpl').tmpl(responseData).appendTo('#table1Data');
+            }
+        );
+        e.preventDefault();
+    });
 }
